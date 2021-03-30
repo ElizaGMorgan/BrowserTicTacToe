2e347853a8f00f4ff117dcccfaef2eeb9c56d783
Elizabeth Morgan <ElizaGMorgan@gmail.com>
2021-03-29 21:40:49 -0500

    Tic tac tie finished

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,62 +1,88 @@
-var gameOver = false;
+var gameOverTie = false;
+var gameOverWinner = false;
 var totalMove = 0;
 var currentPlayer = 'X';
+var player1 = 'X';
+var player2 = 'O';
+
 function placeMarker(spotId){
-    if(!gameOver){
-    //  get the spot/element by spotId
-    var spot = document.getElementById(spotID);
-    //  if the spot is empty
-    //      place the marker by changing the innerHTML of the spot
-    //      depending on who is the current player
-    //      check if current plater has won the game by calling currentPlayerWon()
-    //      if(currentPlayerWon()){
-    //      if yes, display message, set gameOver = true
-    //      }
-    //      if no, switch player, totalMove++
-    // else
-    //     do nothing
-    // else
-    //     the game is over
+  var spot = document.getElementById(spotId);
+    if(!gameOverTie || !gameOverWinner){
+      if(spot.innerHTML == ""){
+        spot.innerHTML = currentPlayer
+        if(currentPlayerWon()){
+          gameOver = true;
+          updateGameStatus()
+        }
+        else{
+          if(currentPlayer == player1){
+            currentPlayer = player2
+            updateGameStatus()
+            totalMove++
+          }
+          else{
+            currentPlayer = player1
+            updateGameStatus()
+            totalMove++
+          }
+        }
+      }
+    }
+    else{
+      updateGameStatus()
     }
 }
 
 function currentPlayerWon(){
-    var s1 = document.getElementById('1');
-    var s2 = document.getElementById('2');
-    var s3 = document.getElementById('3');
-    var s4 = document.getElementById('4');
-    var s5 = document.getElementById('5');
-    var s6 = document.getElementById('6');
-    var s7 = document.getElementById('7');
-    var s8 = document.getElementById('8');
-    var s9 = document.getElementById('9');
-    if(
-      (s1.innterHTML === s2.innerHTML && s2.innerHTML === s3.innerHTML) 
-      || (s4.innterHTML === s5.innerHTML && s5.innerHTML === s6.innerHTML)
-      || (s7.innterHTML === s8.innerHTML && s8.innerHTML === s9.innerHTML)
-      || (s1.innterHTML === s4.innerHTML && s4.innerHTML === s7.innerHTML)
-      || (s2.innterHTML === s5.innerHTML && s5.innerHTML === s8.innerHTML)
-      || (s3.innterHTML === s6.innerHTML && s6.innerHTML === s9.innerHTML)
-      || (s1.innterHTML === s5.innerHTML && s5.innerHTML === s9.innerHTML)
-      || (s3.innterHTML === s5.innerHTML && s5.innerHTML === s7.innerHTML)){
-        gameOver = true
+    var s1 = document.getElementById('space1');
+    var s2 = document.getElementById('space2');
+    var s3 = document.getElementById('space3');
+    var s4 = document.getElementById('space4');
+    var s5 = document.getElementById('space5');
+    var s6 = document.getElementById('space6');
+    var s7 = document.getElementById('space7');
+    var s8 = document.getElementById('space8');
+    var s9 = document.getElementById('space9');
+
+    if((s1.innerHTML == s2.innerHTML && s2.innerHTML == s3.innerHTML && s1.innerHTML != '') 
+      || (s4.innerHTML == s5.innerHTML && s5.innerHTML == s6.innerHTML && s4.innerHTML != '')
+      || (s7.innerHTML == s8.innerHTML && s8.innerHTML == s9.innerHTML && s7.innerHTML != '')
+      || (s1.innerHTML == s4.innerHTML && s4.innerHTML == s7.innerHTML && s1.innerHTML != '')
+      || (s2.innerHTML == s5.innerHTML && s5.innerHTML == s8.innerHTML && s2.innerHTML != '')
+      || (s3.innerHTML == s6.innerHTML && s6.innerHTML == s9.innerHTML && s3.innerHTML != '')
+      || (s1.innerHTML == s5.innerHTML && s5.innerHTML == s9.innerHTML && s1.innerHTML != '')
+      || (s3.innerHTML == s5.innerHTML && s5.innerHTML == s7.innerHTML && s3.innerHTML != ''))
+      {
+        gameOverWinner = true
+        if(currentPlayer == player1){
+          currentPlayer = player2
+        }
+        else{
+          currentPlayer = player1
+        }
       }
     else{
         gameOver = false
     }
     if (!gameOver){
       if (totalMove == 8){
-        gameOver = true;
-      } // maybe 9       
+        gameOverTie = true;
+      }       
     }
     return gameOver
 }
 function updateGameStatus(){
-    if(gameOver){
-        //if it is not a tie, announce the winner
-        //else, announce the game is a tie
+    if(gameOverTie || gameOverWinner){
+      if(gameOverWinner){
+      document.getElementById('announcement').innerHTML = currentPlayer + ' is the winner!'
+       // gameOverWinner = true;
+      }
+      else{
+     document.getElementById('announcement').innerHTML = 'It\'s a tie!'
+      //  gameOver = true;
+      }
     }
     else{
-        //prompt user to make a move
+      document.getElementById('announcement').innerHTML = currentPlayer + ' choose a spot...'
     }
 }
